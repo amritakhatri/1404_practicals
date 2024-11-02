bf17fbde8d90c3e235258dfa719528725b329826
Amrita Khatri <amrita.khatri@my.jcu.edu.au>
2024-11-02 19:49:51 +0800

    If applied, this commit will define the ProgrammingLanguage class with an initializer.

---
+++ prac_06/programming_language.py
@@ -0,0 +1,15 @@
+"""Programming Language class for storing details about programming languages.
+Estimated time:
+Start time: 19:40
+End time:
+"""
+
+class ProgrammingLanguage:
+    """Represent a programming language with typing, reflection capability, and year of creation."""
+
+    def __init__(self, name, typing, reflection, year):
+        """Initialize a ProgrammingLanguage instance."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
