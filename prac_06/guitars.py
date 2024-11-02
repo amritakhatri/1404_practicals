cf324a538e0c8d648f5412c96811599675b27af4
Amrita Khatri <amrita.khatri@my.jcu.edu.au>
2024-11-02 22:29:38 +0800

    If applied, this commit will add code to display the details of each guitar.

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -17,3 +17,13 @@ def main():
     # Preload sample guitars (for testing purposes)
     guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    # Display guitars
+    print("These are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = " (vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+
+if __name__ == "__main__":
+    main()
