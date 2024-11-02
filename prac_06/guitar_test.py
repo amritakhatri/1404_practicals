cb186c1f88040a061356c9fddfd812b61080edf9
Amrita Khatri <amrita.khatri@my.jcu.edu.au>
2024-11-02 21:32:30 +0800

    If applied, this commit will add test cases to verify the is_vintage method of the Guitar class.

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -9,3 +9,11 @@ def main():
     # Test get_age
     print(f"{gibson.name} get_age() - Expected 100. Got {gibson.get_age()}")
     print(f"{another_guitar.name} get_age() - Expected 9. Got {another_guitar.get_age()}")
+
+    # Test is_vintage
+    print(f"{gibson.name} is_vintage() - Expected True. Got {gibson.is_vintage()}")
+    print(f"{another_guitar.name} is_vintage() - Expected False. Got {another_guitar.is_vintage()}")
+
+
+if __name__ == "__main__":
+    main()
