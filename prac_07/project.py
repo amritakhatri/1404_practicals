6ed2c20cd7d84917c85f0331034f3fce430aec54
Amrita Khatri <amrita.khatri@my.jcu.edu.au>
2024-11-09 15:38:16 +0800

    If applied, this commit will create the Project class with attributes and __str__ method

---
+++ prac_07/project.py
@@ -0,0 +1,17 @@
+
+class Project:
+    """A class to represent a project with relevant attributes."""
+
+    def __init__(self, name, start_date, priority, estimate, completion):
+        """Initializes a new Project instance."""
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.estimate = estimate
+        self.completion = completion
+
+    def __str__(self):
+        """Returns a string representation of a project."""
+        return f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.estimate}, completion: {self.completion}%"
+
+
