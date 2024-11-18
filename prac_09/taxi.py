e95118fb6e3f27cd1bc7e231b89cf3b7a01ff5d3
amritakhatri <amrita.khatri@my.jcu.edu.au>
2024-11-18 17:26:48 +0800

    If applied, this commit will add methods for calculating fares, resetting fares, and overriding the drive method in Taxi.

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -14,3 +14,17 @@ class Taxi(Car):
         """Initialise a Taxi instance, based on parent class Car."""
         super().__init__(name, fuel)
         self.current_fare_distance = 0
+
+    def get_fare(self):
+        """Return the price for the taxi trip."""
+        return self.price_per_km * self.current_fare_distance
+
+    def start_fare(self):
+        """Begin a new fare."""
+        self.current_fare_distance = 0
+
+    def drive(self, distance):
+        """Drive like parent Car but calculate fare distance as well."""
+        distance_driven = super().drive(distance)
+        self.current_fare_distance += distance_driven
+        return distance_driven
