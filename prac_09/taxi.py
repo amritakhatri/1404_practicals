3952074acdd3a7d2f059f85a2a44769ccf6dc231
amritakhatri <amrita.khatri@my.jcu.edu.au>
2024-11-18 17:27:52 +0800

    If applied, this commit will override the __str__ method in Taxi for a detailed string representation.

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -15,16 +15,23 @@ class Taxi(Car):
         super().__init__(name, fuel)
         self.current_fare_distance = 0
 
+
     def get_fare(self):
         """Return the price for the taxi trip."""
         return self.price_per_km * self.current_fare_distance
 
+
     def start_fare(self):
         """Begin a new fare."""
         self.current_fare_distance = 0
+
 
     def drive(self, distance):
         """Drive like parent Car but calculate fare distance as well."""
         distance_driven = super().drive(distance)
         self.current_fare_distance += distance_driven
         return distance_driven
+
+    def __str__(self):
+        """Return a string like a Car but with current fare distance."""
+        return f"{super().__str__()}, {self.current_fare_distance}km on current fare, ${self.price_per_km:.2f}/km"
