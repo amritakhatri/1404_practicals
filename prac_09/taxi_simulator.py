f11ebfadd595c79698c50020c130462e80fee3df
amritakhatri <amrita.khatri@my.jcu.edu.au>
2024-11-18 17:39:41 +0800

    If applied, this commit will create the main function and import required modules for the Taxi Simulator.

---
+++ prac_09/taxi_simulator.py
@@ -0,0 +1,23 @@
+"""
+CP1404/CP5632 Practical
+Taxi simulator program
+"""
+
+from taxi import Taxi
+from silver_service_taxi import SilverServiceTaxi
+
+MENU = "q)uit, c)hoose taxi, d)rive"
+
+
+def main():
+    """Taxi simulator program."""
+    print("Let's drive!")
+    print(MENU)
+    taxis = [
+        Taxi("Prius", 100),
+        SilverServiceTaxi("Limo", 100, 2),
+        SilverServiceTaxi("Hummer", 200, 4)
+    ]
+    current_taxi = None
+    total_bill = 0.0
+    print(">>> ", end="")
