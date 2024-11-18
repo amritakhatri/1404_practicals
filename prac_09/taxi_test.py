580ee543fd704b15b3b11d3bbb1dc209e07da775
amritakhatri <amrita.khatri@my.jcu.edu.au>
2024-11-18 17:29:58 +0800

    If applied, this commit will add tests for restarting the fare and driving additional distances.

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -16,3 +16,14 @@ def main():
     # Print the taxi's details and the current fare
     print(my_taxi)
     print(f"Current fare: ${my_taxi.get_fare():.2f}")
+
+    # Restart the meter and drive 100 km
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+
+    # Print the details and the current fare
+    print(my_taxi)
+    print(f"Current fare: ${my_taxi.get_fare():.2f}")
+
+if __name__ == "__main__":
+    main()
