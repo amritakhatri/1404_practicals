b3f51a83ad523f622a23d36c041d40f163bcdd20
amritakhatri <amrita.khatri@my.jcu.edu.au>
2024-11-23 23:40:18 +0800

    If applied, this commit will implement price adjustment using fanciness for the SilverServiceTaxi class.

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -8,3 +8,15 @@ class SilverServiceTaxi(Taxi):
         """Initialise a SilverServiceTaxi object."""
         super().__init__(name, fuel)
         self.fanciness = fanciness
+
+from taxi import Taxi
+
+class SilverServiceTaxi(Taxi):
+    """A SilverServiceTaxi is a special type of Taxi with higher pricing."""
+    flagfall = 4.50
+
+    def __init__(self, name, fuel, fanciness):
+        """Initialise a SilverServiceTaxi object."""
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km = Taxi.price_per_km * self.fanciness
