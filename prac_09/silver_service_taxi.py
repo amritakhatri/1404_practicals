1879bc8241a787be82f4f701246fe94d8e00073a
amritakhatri <amrita.khatri@my.jcu.edu.au>
2024-11-23 23:41:05 +0800

    If applied, this commit will override the __str__ method in the SilverServiceTaxi class.

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -24,3 +24,7 @@ class SilverServiceTaxi(Taxi):
 def get_fare(self):
     """Calculate the fare including the flagfall."""
     return super().get_fare() + self.flagfall
+
+def __str__(self):
+    """Return a string representation of the SilverServiceTaxi."""
+    return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
