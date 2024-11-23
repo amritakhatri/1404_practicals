82ce14af238e4d0eb5dadafee9e95970ed3cd02f
amritakhatri <amrita.khatri@my.jcu.edu.au>
2024-11-23 23:42:10 +0800

    If applied, this commit will implement test setup for the SilverServiceTaxi class.

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -1,3 +1,12 @@
 def test_silver_service_taxi():
     """Test the SilverServiceTaxi class."""
     pass
+
+from silver_service_taxi import SilverServiceTaxi
+
+def test_silver_service_taxi():
+    """Test the SilverServiceTaxi class."""
+    # Create a SilverServiceTaxi with fanciness level 2
+    taxi = SilverServiceTaxi("Limo", 100, 2)
+    taxi.start_fare()
+
