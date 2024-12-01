6420d22686448db450aa06f2277ed95a95d21111
Amrita Khatri <amrita.khatri@my.jcu.edu.au>
2024-12-01 08:52:18 +0800

    If applied, this commit will enable doctests by uncommenting doctest.testmod()

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -9,12 +9,12 @@ from prac_06.car import Car
 
 def repeat_string(s, n):
     """Repeat string s, n times, with spaces in between."""
-    return s * n
+    return " ".join([s] * n)
 
 
 def is_long_word(word, length=5):
     """
-    Determine if the word is as long or longer than the length passed in
+    Determine if the word is as long or longer than the length passed in.
     >>> is_long_word("not")
     False
     >>> is_long_word("supercalifrag")
@@ -22,7 +22,8 @@ def is_long_word(word, length=5):
     >>> is_long_word("Python", 6)
     True
     """
-    return len(word) > length
+    return len(word) >= length
+
 
 def format_sentence(phrase):
     """
@@ -36,25 +37,13 @@ def format_sentence(phrase):
     """
     return phrase.capitalize().rstrip('.') + '.'
 
+
 def run_tests():
     """Run the tests on the functions."""
-    # assert test with no message - used to see if the function works properly
+    # Test repeat_string function
     assert repeat_string("Python", 1) == "Python"
-    # the test below should fail
     assert repeat_string("hi", 2) == "hi hi"
 
- def repeat_string(s, n):
-        """Repeat string s, n times, with spaces in between."""
-    return " ".join([s] * n)
-
-    # assert test with custom message,
-    # used to see if Car's init method sets the odometer correctly
-    # this should pass (no output)
-    car = Car()
-    assert car._odometer == 0, "Car does not set odometer correctly"
-
-def run_tests():
-    """Run the tests on the functions."""
     # Test Car class functionality
     car_default = Car()
     assert car_default.fuel == 0, "Car does not default fuel to 0"
@@ -62,13 +51,8 @@ def run_tests():
     car_custom = Car(fuel=10)
     assert car_custom.fuel == 10, "Car does not set custom fuel correctly"
 
-    # Note that Car's __init__ function sets the fuel in one of two ways:
-    # using the value passed in or the default
-    # You should test both of these
-    car = Car(fuel=10)
-
 
 run_tests()
 
+# Run doctests
 doctest.testmod()
-
